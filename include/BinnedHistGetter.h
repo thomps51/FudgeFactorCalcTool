76ba8d0966d67a7b4c046c541eead09b3c02e983
Tony Thompson <ajthomps51@gmail.com>
2016-04-11 13:56:34 -0400

    added map, removed hardcoded variables, added ConvStatus

--- include/BinnedHistGetter.h
+++ include/BinnedHistGetter.h
@@ -11,42 +11,46 @@ using namespace std;
 class BinnedHistGetter
 {
   public:
-    BinnedHistGetter(string inputFile,string data_type);
+    BinnedHistGetter(string inputFile,string convSt, string data_type);
     int main(string file);
     void makeHistos(); 
     void init(); 
     void LoopOverFile();
+    void writeHistsToFile();
     string inputFileName; 
     string dataType; // mc or data
+    string convStatus; // u or c
     vector<vector<vector<TH1F * > > > getHists(){
       init();
       makeHistos();
       LoopOverFile();
       return hists;
     }
+    void Run()
+    {
+      init();
+      makeHistos();
+      LoopOverFile();
+      writeHistsToFile();
+    }
     
     TFile *f;
     TTree *ttree;
 
     vector<vector<vector<TH1F * > > > hists;  // 3d vector in form [var][etBin][etaBin]
-    //vector<vector<string > > histnames;
-
-
-   // int etBins[11] = {8,15,20,25,30,40,50,60,80,100,1000};
-   // float etaBins[9]= {0.00,0.60,0.80,1.15,1.37,1.52,1.81,2.01,2.37};
-
+    map<string,vector<float> * > inputVars;
 
     vector<float> * ph_pt;
     vector<float> * ph_eta;
     vector<float> *ph_phi;
-    vector<float> *ph_reta;
-    vector<float> *ph_rphi;
-    vector<float> *ph_weta2;
-    vector<float> *ph_w1;
-    vector<float> *ph_wstot;
-    vector<float> *ph_fside;
-    vector<float> *ph_rhad;
-    vector<float> *ph_rhad1;
+    //vector<float> *ph_reta;
+    //vector<float> *ph_rphi;
+    //vector<float> *ph_weta2;
+    //vector<float> *ph_w1;
+    //vector<float> *ph_wstot;
+    //vector<float> *ph_fside;
+    //vector<float> *ph_rhad;
+    //vector<float> *ph_rhad1;
     vector<float> *ph_weight;
 
 
