fc49d9597231d5994a1f2901d74afc0cec8922f2
Tony Thompson <ajthomps51@gmail.com>
2016-04-12 13:43:33 -0400

    formatting fixes

--- utils/compare.C
+++ utils/compare.C
@@ -1,7 +1,4 @@
-#include "include/BinnedHistGetter.h"
-#include "include/BinnedHistGetterNTUP.h"
 #include "getopt.h"
-#include "include/PDFgetter.h"
 #include "include/FFcalcAndCompare.h"
 #include "TMVA/PDF.h"
 using namespace std;
@@ -11,13 +8,10 @@ int main(int argc, char **argv)
 {
   FFcalcAndCompare * FFobj_c = new FFcalcAndCompare("output/pdfs/pdfs_data_c.root","output/pdfs/pdfs_mc_c.root","c");
   FFobj_c->Run();
-  //cout << "test2" << endl;
-  //delete FFobj_c;
 
 
-  //FFcalcAndCompare * FFobj_u = new FFcalcAndCompare("output/pdfs_data_u.root","output/pdfs_mc_u.root","u");
-  //FFobj_u->Run();
-  //delete FFobj_u;
+  FFcalcAndCompare * FFobj_u = new FFcalcAndCompare("output/pdfs/pdfs_data_u.root","output/pdfs/pdfs_mc_u.root","u");
+  FFobj_u->Run();
 
 
 }
