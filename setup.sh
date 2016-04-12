65ea6cd8779913629cce46a5ce21d38f4ab77c56
Tony Thompson <ajthomps51@gmail.com>
2016-04-12 13:42:46 -0400

    added directory checks for portability

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
+histDir=output/hists
+pdfsDir=output/pdfs
+[[ ! -d "$histDir" ]] && echo "default output directory $histDir not made, making..." && mkdir -p $histDir
+[[ ! -d "$pdfsDir" ]] && echo "default output directory $pdfsDir not made, making..." && mkdir -p $pdfsDir
+[[ ! -d libs ]] && mkdir libs
 setupATLAS
 localSetupROOT     # definitely working with 6.04.14-x86_64-slc6-gcc49-opt
+
+
+
