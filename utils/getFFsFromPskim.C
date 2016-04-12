eaed2643d818ad10de5befff59378eaaf73649fe
Tony Thompson <ajthomps51@gmail.com>
2016-04-12 13:44:09 -0400

    clean up running methods

--- utils/getFFsFromPskim.C
+++ utils/getFFsFromPskim.C
@@ -1,5 +1,4 @@
 #include "include/BinnedHistGetter.h"
-#include "include/BinnedHistGetterNTUP.h"
 #include "getopt.h"
 #include "include/PDFgetter.h"
 #include "include/FFcalc.h"
@@ -11,29 +10,31 @@ using namespace std;
 int main(int argc, char **argv)
 {
   //CONVERTED
-  // make binned histograms in et and eta of the MC converted photons
+  //make binned histograms in et and eta of the MC converted photons
   cout << "making converted MC binned histograms" << endl;
-  BinnedHistGetter mc_conv(Config::inputPskimmedPathMCconv.c_str(),"mc");
-  vector<vector<vector<TH1F * > > > mcConvHists = mc_conv.getHists();
+  BinnedHistGetter mc_conv(Config::inputPskimmedPathMCconv.c_str(),"c","mc");
+  mc_conv.Run();
+  
+  //vector<vector<vector<TH1F * > > > mc_conv_hists = mc_conv.getHists();
+
   cout << "got conv MC hists" << endl << endl;
   cout << "making MC PDFs...." << endl<< endl;
-  PDFgetter mc_conv_PDFs_obj(mcConvHists,"c","mc");
-  vector<vector<vector<TH1 * > > > mc_conv_PDFs = mc_conv_PDFs_obj.makeSmoothedPDFs(); 
-  mc_conv_PDFs_obj.writeToFile();
-
+  
+  PDFgetter mc_conv_PDFs_obj("output/hists/hists_mc_c.root","c","mc");
+  //PDFgetter mc_conv_PDFs_obj(mc_conv_hists,"c","mc");
+  mc_conv_PDFs_obj.Run();
 
   cout << "making converted data binned histograms" << endl;
-  BinnedHistGetter data_conv(Config::inputPskimmedPathDataConv.c_str(),"data");
-  vector<vector<vector<TH1F * > > > dataConvHists = data_conv.getHists();
+  BinnedHistGetter data_conv(Config::inputPskimmedPathDataConv.c_str(),"c","data");
+  data_conv.Run();
   cout << "got conv Data hists" << endl;
   cout << "making conv Data PDFs...." << endl<< endl;
-  PDFgetter data_conv_PDFs_obj(dataConvHists,"c","data");
-  vector<vector<vector<TH1 * > > > data_conv_PDFs = data_conv_PDFs_obj.makeSmoothedPDFs(); 
-  data_conv_PDFs_obj.writeToFile(); 
+  
+  PDFgetter data_conv_PDFs_obj("output/hists/hists_data_c.root","c","data");
+  data_conv_PDFs_obj.Run();
   cout << "making fudge factors for converted photons...." << endl<< endl;
 
-  FFcalc FFobj_c(data_conv_PDFs,mc_conv_PDFs,"c"); 
-  //FFcalc FFobj("output/pdfs_data_c.root","output/pdfs_mc_c.root","c"); 
+  FFcalc FFobj_c("output/pdfs/pdfs_data_c.root","output/pdfs/pdfs_mc_c.root","c"); 
   FFobj_c.Run(); 
   cout << "Converted Fudge Factors are made, check output/ folder" << endl<< endl;
   
@@ -41,31 +42,28 @@ int main(int argc, char **argv)
   // make binned histograms in et and eta of the MC converted photons
   cout << "making unconverted MC binned histograms" << endl;
 
-  BinnedHistGetter mc_unconv(Config::inputPskimmedPathMCunconv.c_str(),"mc");
-  vector<vector<vector<TH1F * > > > mcUnconvHists = mc_unconv.getHists();
+  BinnedHistGetter mc_unconv(Config::inputPskimmedPathMCunconv.c_str(),"u","mc");
+  mc_unconv.Run();
   
   cout << "got unconv MC hists" << endl << endl;
   cout << "making unconv MC PDFs...." << endl<< endl;
   
-  PDFgetter mc_unconv_PDFs_obj(mcUnconvHists,"u","mc");
-  vector<vector<vector<TH1 * > > > mc_unconv_PDFs = mc_unconv_PDFs_obj.makeSmoothedPDFs(); 
-  mc_unconv_PDFs_obj.writeToFile(); 
-  
+  PDFgetter mc_unconv_PDFs_obj("output/hists/hists_mc_u.root","u","mc");
+  mc_unconv_PDFs_obj.Run();
+
   cout << "getting unconv data hists" << endl << endl;
-  BinnedHistGetter data_unconv(Config::inputPskimmedPathDataUnconv.c_str(),"data");
-  vector<vector<vector<TH1F * > > > dataUnconvHists = data_unconv.getHists();
-  
+  BinnedHistGetter data_unconv(Config::inputPskimmedPathDataUnconv.c_str(),"u","data");
+  data_unconv.Run();
+
   cout << "got unconv Data hists" << endl;
   cout << "making unconv Data PDFs...." << endl<< endl;
   
-  PDFgetter data_unconv_PDFs_obj(dataUnconvHists,"u","data");
-  vector<vector<vector<TH1 * > > > data_unconv_PDFs = data_unconv_PDFs_obj.makeSmoothedPDFs(); 
-  data_unconv_PDFs_obj.writeToFile(); 
-  
+  PDFgetter data_unconv_PDFs_obj("output/hists/hists_data_u.root","u","data");
+  data_unconv_PDFs_obj.Run();
+
   cout << "making fudge factors for unconverted photons...." << endl;
 
-  FFcalc FFobj_u(data_unconv_PDFs,mc_unconv_PDFs,"u"); 
-  //FFcalc FFobj("output/pdfs_data_c.root","output/pdfs_mc_c.root","c"); 
+  FFcalc FFobj_u("output/pdfs/pdfs_data_u.root","output/pdfs/pdfs_mc_u.root","u"); 
   FFobj_u.Run(); 
   cout << "Unconverted Fudge Factors are made, check output/ folder" << endl;
 
