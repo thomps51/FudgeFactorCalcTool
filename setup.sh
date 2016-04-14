2c825658d617dd37fa385cd967b7fbcde7512b84
Tony Thompson <ajthomps51@gmail.com>
2016-04-14 13:49:50 -0400

    moved output directory creation to individual objects

--- setup.sh
+++ setup.sh
@@ -1,10 +1,4 @@
-histDir=output/hists
-pdfsDir=output/pdfs
-skimDir=output/skimmedNtups
-DIRS=(libs output/hists output/pdfs output/skimmedNtups)
-for DIR in ${DIRS[@]}; do
-  [[ ! -d "$DIR" ]] && echo "default output directory $DIR not made, making..." && mkdir -p $DIR
-done
+[[ ! -d "libs" ]] && echo "library directory libs/ not made, making..." && mkdir -p libs
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 lsetup root     # definitely working with 6.04.14-x86_64-slc6-gcc49-opt
