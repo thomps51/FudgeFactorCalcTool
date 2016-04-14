38f0b9e115e65e1a090159203fa9e8e68e7f34e9
Tony Thompson <ajthomps51@gmail.com>
2016-04-14 11:47:41 -0400

    cleanup

--- utils/getFFsFromPDFs.C
+++ utils/getFFsFromPDFs.C
@@ -1,9 +1,5 @@
-#include "include/BinnedHistGetter.h"
-#include "include/BinnedHistGetterNTUP.h"
 #include "getopt.h"
-#include "include/PDFgetter.h"
 #include "include/FFcalc.h"
-#include "TMVA/PDF.h"
 using namespace std;
 
 
@@ -11,13 +7,7 @@ int main(int argc, char **argv)
 {
   FFcalc * FFobj_c = new FFcalc("output/pdfs/pdfs_data_c.root","output/pdfs/pdfs_mc_c.root","c");
   FFobj_c->Run();
-  //cout << "test2" << endl;
-  //delete FFobj_c;
-
 
   FFcalc * FFobj_u = new FFcalc("output/pdfs/pdfs_data_u.root","output/pdfs/pdfs_mc_u.root","u");
   FFobj_u->Run();
-  //delete FFobj_u;
-
-
 }
