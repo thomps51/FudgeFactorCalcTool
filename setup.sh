7568448f3804ac9e04c56d56cf006a36bdc0588f
Tony Thompson <ajthomps51@gmail.com>
2016-04-14 11:47:22 -0400

    cleaned up directory creation, explicit setupATLAS

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 histDir=output/hists
 pdfsDir=output/pdfs
-[[ ! -d "$histDir" ]] && echo "default output directory $histDir not made, making..." && mkdir -p $histDir
-[[ ! -d "$pdfsDir" ]] && echo "default output directory $pdfsDir not made, making..." && mkdir -p $pdfsDir
-[[ ! -d libs ]] && mkdir libs
-setupATLAS
-localSetupROOT     # definitely working with 6.04.14-x86_64-slc6-gcc49-opt
+skimDir=output/skimmedNtups
+DIRS=(libs output/hists output/pdfs output/skimmedNtups)
+for DIR in ${DIRS[@]}; do
+  [[ ! -d "$DIR" ]] && echo "default output directory $DIR not made, making..." && mkdir -p $DIR
+done
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+lsetup root     # definitely working with 6.04.14-x86_64-slc6-gcc49-opt
 
 
 
