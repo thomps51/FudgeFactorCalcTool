0f4c44f6c19bca1fcaf5ca9f3f8ec796df6ace08
Tony Thompson <ajthomps51@gmail.com>
2016-04-14 11:45:48 -0400

    added filename variables, added mkpath function to make paths

--- include/Config.h
+++ include/Config.h
@@ -1,10 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 #include <string>
+#include <sys/stat.h>
 using namespace std;
 class Config{
   public:
-// INPUT TYPE 1: NTUPLES from egammaAODtoNtupleDumper
+// INPUT: NTUPLES from egammaAODtoNtupleDumper
     static string inputDumpedNTUPpathData    ;    
     static string inputDumpedNTUPpathMC      ;   
     static bool   use_noFF_vars;
@@ -12,11 +13,6 @@ class Config{
     static string InputVarsNTUP [];
     static string InputVarsNoFFNTUP [];
 
-// INPUT TYPE 2: "Pskimmed files"
-    static string inputPskimmedPathDataConv  ;  
-    static string inputPskimmedPathDataUnconv;
-    static string inputPskimmedPathMCconv    ;
-    static string inputPskimmedPathMCunconv  ;
     static string InputVarsPskim [];
     static int    NvarsInputPskim;
 
@@ -24,9 +20,24 @@ class Config{
     static int    Nbins;
     static int    shift_max;
     static int    shift_min;
+    static string skimOutputDir;
+    static string skimMCconvFile;     
+    static string skimMCunconvFile;   
+    static string skimDataConvFile;   
+    static string skimDataUnconvFile;
+    
     static string FFsOutputDir;
     static string histOutputDir;
+    static string histMCconvFile;
+    static string histMCunconvFile;
+    static string histDataConvFile;
+    static string histDataUnconvFile;
+    
     static string pdfsOutputDir;
+    static string pdfsMCconvFile;
+    static string pdfsMCunconvFile;
+    static string pdfsDataConvFile;
+    static string pdfsDataUnconvFile;
     static string varsN[];
     static float  xmins[];
     static float  xmaxs[];
@@ -38,6 +49,27 @@ class Config{
     static int    NetBins;
     static int    NetaBins;
     static int    Nvars;
+    
+    static int    mkpath(std::string s,mode_t mode){
+      size_t pre=0,pos;
+      std::string dir;
+      int mdret=0;
 
+      if(s[s.size()-1]!='/'){
+          // force trailing / so we can handle everything in loop
+          s+='/';
+      }
+
+      while((pos=s.find_first_of('/',pre))!=std::string::npos){
+          dir=s.substr(0,pos++);
+          pre=pos;
+          if(dir.size()==0) continue; // if leading / first time is 0 length
+          if((mdret=mkdir(dir.c_str(),mode)) && errno!=EEXIST){
+              return mdret;
+          }
+      }
+      return mdret;
+
+    }
 };
 #endif
