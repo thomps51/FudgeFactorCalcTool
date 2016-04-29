c4f265d648843fae40ac1703067e82c13ec0123b
Tony Thompson <ajthomps51@gmail.com>
2016-04-29 18:31:50 -0400

    fixing libs/ creation and setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,3 @@
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 lsetup root     # definitely working with 6.04.14-x86_64-slc6-gcc49-opt
-
-
-
